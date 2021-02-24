abadbb7de1b7461d3e3675c7fc948d38a7206618
Burhanuddin Udaipurwala <udaipurwalaburhanuddin@gmail.com>
2021-02-24 10:17:27 +0530

    conversions between data types

---
+++ ruby/c.conversions.rb
@@ -0,0 +1,10 @@
+# To convert an integer to a float:
+13.to_f   #=> 13.0
+
+# To convert a float to an integer:
+13.0.to_i #=> 13
+13.9.to_i #=> 13
+
+5.to_s        #=> "5"
+nil.to_s      #=> ""
+:symbol.to_s  #=> "symbol"
