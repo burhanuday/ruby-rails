fc602bad3c77af9ec5b1f7728875ab397d571067
Burhanuddin Udaipurwala <udaipurwalaburhanuddin@gmail.com>
2021-02-24 10:17:15 +0530

    basic data types

---
+++ ruby/a.basic-data-types.rb
@@ -0,0 +1,28 @@
+# Addition
+1 + 1   #=> 2
+
+# Subtraction
+2 - 1   #=> 1
+
+# Multiplication
+2 * 2   #=> 4
+
+# Division
+10 / 5  #=> 2
+
+# Exponent
+2 ** 2  #=> 4
+3 ** 4  #=> 81
+
+# Modulus (find the remainder of division)
+8 % 2   #=> 0  (8 / 2 = 4; no remainder)
+10 % 4  #=> 2  (10 / 4 = 2 with a remainder of 2)
+
+17 / 5    #=> 3, not 3.4
+17 / 5.0  #=> 3.4
+
+6.even? #=> true
+7.even? #=> false
+
+6.odd? #=> false
+7.odd? #=> true
